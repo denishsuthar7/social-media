fca74f82fdc4d39f06d060d739d2f25b54676c75
sknetking <skentkingseo@gmail.com>
2023-07-08 11:27:08 +0530

    v1

---
+++ routes/userRoute.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { allUsers, deleteUser, findUser, followAndUnfollowUser, forgotPassword, login, logout, myProfile, register, resetPassword, updatePassword, updateProfile, updateRole, verifyEmail } from "../controller/userController.js";
+import { isAdmin, isAuthenticatedUser } from "../middelware/auth.js";
+import singleUpload from "../middelware/multer.js";
+
+
+const router = express.Router();
+
+router.route("/register").post(singleUpload,register)
+
+router.route("/login").post(login)
+
+router.route("/logout").get(logout)
+
+router.route("/password/forgot").post(forgotPassword)
+
+router.route("/password/reset/:token").put(resetPassword)
+
+router.route("/me").get(isAuthenticatedUser,myProfile)
+
+router.route("/me/update").put(isAuthenticatedUser, singleUpload, updateProfile)
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
+
+router.route("/verify/:token").get(verifyEmail)
+
+router.route("/follow/:id").get(isAuthenticatedUser, followAndUnfollowUser)
+
+router.route("/users").get(isAuthenticatedUser,isAdmin,allUsers)
+
+router.route("/user/:userName").get(isAuthenticatedUser, findUser)
+
+router.route("/user/:id").delete(isAuthenticatedUser, isAdmin, deleteUser).put(isAuthenticatedUser, isAdmin, updateRole)
+
+
+export default router
