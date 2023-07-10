5211d759f4caa9f9810b9dc26903cdfdbca3b3a7
denishsuthar <denish.v2web@gmail.com>
2023-07-10 18:10:19 +0530

    cors

--- app.js
+++ app.js
@@ -16,17 +16,6 @@ app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(cors())
 
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-
-app.use(allowCrossDomain);
-
 // Config
 config({
     path:"./config/config.env"
